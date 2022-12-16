fa48b4e3eef862e23a8185a6d91b378984d49805
DESKTOP-B1QD8OP\hoang <hoang.nguyen@molinetwork.com>
2022-12-16 15:38:59 +0700

    Add main function

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from v3youtube.rapid import RapidApi
 
 import time
 
-start_time = time.time()
-f = open("accounts.txt", "r")
-for email in f:
-    RapidApi(email)
-        
 
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == '__main__':
+    start_time = time.time()
+    f = open("accounts.txt", "r")
+    for email in f:
+        RapidApi(email)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
